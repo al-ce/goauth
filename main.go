864c836b6d8ace0d82ad495d03aed4b5af29a64f
al-ce <aceserani@gmail.com>
2025-03-22 17:17:23 -0500

    feat(gin): ping home

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 
 	"gofit/initializers"
@@ -15,4 +17,12 @@ func init() {
 
 func main() {
 	log.Info().Str("PORT", os.Getenv("PORT")).Msg("Starting server")
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
+	})
+
+	r.Run()
 }
