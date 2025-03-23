776ed586b16fa429846843b7a46e8f8e9494d295
al-ce <aceserani@gmail.com>
2025-03-23 01:11:34 -0500

    chore(migrations): use ticks for string formatting

--- internal/database/migrations.go
+++ internal/database/migrations.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Migrate(db *gorm.DB) error {
-	if err := db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";").Error; err != nil {
+	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`).Error; err != nil {
 		log.Fatal().Err(err).Msg("Error migrating database")
 	}
 
