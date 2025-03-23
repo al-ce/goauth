f37fe91d74cce4802fe893fe9b338800da046029
al-ce <aceserani@gmail.com>
2025-03-23 15:20:06 -0500

    test(userHandler): update login test cases

--- internal/handlers/userHandler_test.go
+++ internal/handlers/userHandler_test.go
@@ -2,8 +2,10 @@ package handlers_test
 
 import (
 	"net/http"
+	"os"
 	"testing"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/matryer/is"
 
 	"gofit/internal/server"
@@ -21,8 +23,9 @@ func TestUserHandler_RegisterUser(t *testing.T) {
 	server.SetupRoutes()
 
 	validEmail := "testRegisterUser@test.com"
-	validPassword := "correcthorsebatterystable" // strong password for validator
+	validPassword := "correcthorsebatterystaple" // strong password for validator
 
+	// Test cases
 	testCases := []struct {
 		name     string
 		req      Request
@@ -69,28 +72,66 @@ func TestUserHandler_Login(t *testing.T) {
 	server := server.NewAPIServer(tx)
 	server.SetupRoutes()
 
-	validEmail := "testRegisterUser@test.com"
-	validPassword := "correcthorsebatterystable" // strong password for validator
+	validEmail := "testUserHandlerLoginUser@test.com"
+	validPassword := "correcthorsebatterystaple" // strong password for validator
+	validEmail2 := "SECONDARYtestUserHandlerLoginUser@test.com"
+	validPassword2 := "SECONDARYcorrecthorsebatterystaple"
 
+	// Register first user
+	rr, err := testutils.MakeRequest(
+		server.Router,
+		"POST",
+		"/register",
+		Request{Email: validEmail, Password: validPassword},
+	)
+	is.NoErr(err)
+	is.Equal(rr.Code, http.StatusOK)
+
+	// Register second user
+	rr, err = testutils.MakeRequest(
+		server.Router,
+		"POST",
+		"/register",
+		Request{Email: validEmail2, Password: validPassword2},
+	)
+	is.NoErr(err)
+	is.Equal(rr.Code, http.StatusOK)
+
+	// Test cases
 	testCases := []struct {
 		name     string
 		req      Request
 		expected int
 	}{
 		{
-			name:     "valid request",
-			req:      Request{Email: validEmail, Password: validPassword},
-			expected: http.StatusOK,
-		},
-		{
-			name:     "invalid request no email",
+			name:     "no email",
 			req:      Request{Password: "password"},
 			expected: http.StatusBadRequest,
 		},
 		{
-			name:     "invalid request no password",
+			name:     "no password",
 			req:      Request{Email: "some@test.com"},
 			expected: http.StatusBadRequest,
+		},
+		{
+			name:     "non-existent user",
+			req:      Request{Email: "doesNotExist@test.com", Password: validPassword},
+			expected: http.StatusBadRequest,
+		},
+		{
+			name:     "incorrect password",
+			req:      Request{Email: validEmail, Password: "thisIsNotThePassword"},
+			expected: http.StatusBadRequest,
+		},
+		{
+			name:     "existing password, mismatched existing user",
+			req:      Request{Email: validEmail, Password: validPassword2},
+			expected: http.StatusBadRequest,
+		},
+		{
+			name:     "valid request",
+			req:      Request{Email: validEmail, Password: validPassword},
+			expected: http.StatusOK,
 		},
 	}
 
@@ -104,6 +145,28 @@ func TestUserHandler_Login(t *testing.T) {
 			)
 			is.NoErr(err)
 			is.Equal(rr.Code, tc.expected)
+
+			// Check cookie is set on valid request, and is valid JWT
+			if tc.name == "valid request" && tc.expected == http.StatusOK {
+				cookies := rr.Result().Cookies()
+				var jwtCookie *http.Cookie
+				for _, cookie := range cookies {
+					if cookie.Name == "JWT_SESSION" {
+						jwtCookie = cookie
+						break
+					}
+				}
+				is.True(jwtCookie != nil)
+
+				tokenString := jwtCookie.Value
+
+				parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+					return []byte(os.Getenv("JWT_SECRET")), nil
+				}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+
+				is.NoErr(err)
+				is.True(parsedToken.Valid)
+			}
 		})
 	}
 }
