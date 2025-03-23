e33445dde534d20b01bf8d8fef47ee261091b92c
al-ce <aceserani@gmail.com>
2025-03-23 01:12:47 -0500

    feat(tests): silence gorm logger

--- internal/testutils/testHelpers.go
+++ internal/testutils/testHelpers.go
@@ -1,9 +1,15 @@
 package testutils
 
 import (
+	"io"
+	"log"
 	"os"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 
 	"gofit/internal/database"
 )
@@ -11,10 +17,28 @@ import (
 func TestEnvSetup() {
 	os.Setenv("PORT", "3001")
 	os.Setenv("DB", "host=localhost user=gofit_test password=gofit_test dbname=gofit_test port=5432 sslmode=disable TimeZone=UTC")
+
+	gin.SetMode(gin.TestMode)
+	gin.DefaultWriter = io.Discard
 }
 
 func TestDBSetup() *gorm.DB {
-	db := database.NewDB()
+	gormLogger := logger.New(
+		log.New(io.Discard, "", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Silent,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  false,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB")), &gorm.Config{
+		Logger: gormLogger,
+	})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
 
 	database.Migrate(db)
 	return db
