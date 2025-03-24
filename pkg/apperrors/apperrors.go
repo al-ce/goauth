b1714929a3c0e101e00f86a9343dd72b6b2a22dd
al-ce <aceserani@gmail.com>
2025-03-23 19:10:27 -0500

    feat(apperrors): add erruseridempty

--- pkg/apperrors/apperrors.go
+++ pkg/apperrors/apperrors.go
@@ -15,4 +15,5 @@ var (
 	ErrEmailMaxLength  = New("Email exceeds max length of 254 characters")
 	ErrInvalidLogin    = New("Invalid login credentials")
 	ErrTokenGeneration = New("Could not generate token")
+	ErrUserIdEmpty     = New("User ID is empty")
 )
