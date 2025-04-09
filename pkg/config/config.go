c985622a3335f4d4fb1a0652a78449f777878df4
al-ce <aceserani@gmail.com>
2025-04-09 18:13:38 -0500

    feat(config): add MaxLoginAttempts, AccountLockoutLength

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,3 +5,7 @@ const JwtCookieName = "JWT_SECRET"
 const TokenExpiration = 3600*24*7
 
 const TestingPassword = "correcthorsebatterystaple"
+
+const MaxLoginAttempts = 5
+
+const AccountLockoutLength = 60 * 15
