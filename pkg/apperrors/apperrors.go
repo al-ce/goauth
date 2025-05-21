0cd0a85627b89d8cce46974d4f87b0d9c9de5e05
al-ce <aceserani@gmail.com>
2025-05-21 09:27:31 -0500

    feat(apperrors): add new errors, organize

--- pkg/apperrors/apperrors.go
+++ pkg/apperrors/apperrors.go
@@ -7,21 +7,42 @@ import (
 var New = errors.New
 
 var (
-	ErrAccountIsLocked               = New("Account is locked")
+	// Authentication errors
+	ErrAccountIsLocked     = New("Account is locked")
+	ErrInvalidLogin        = New("Invalid login credentials")
+	ErrInvalidTokenFormat  = New("Invalid token format")
+	ErrSessionIDGeneration = New("Could not generate token")
+
+	// User registration errors
+	ErrDuplicateEmail     = New("User already registered with this email")
+	ErrEmailIsEmpty       = New("Email is empty")
+	ErrEmailMaxLength     = New("Email exceeds max length of 254 characters")
+	ErrEmailFormat        = New("Email format is invalid")
+	ErrPasswordComplexity = New("Please use a more complex password! https://xkcd.com/936")
+
+	// Session errors
+	ErrSessionAlreadyExists = New("Session already exists")
+
+	// Nil reference argument errors
+	ErrDatabaseIsNil     = New("Database is nil")
+	ErrSessionIsNil      = New("Session is nil")
+	ErrUserIsNil         = New("User is nil")
+	ErrSessionRepoIsNil  = New("Session repo is nil")
+	ErrUserRepoIsNil     = New("UserRepo is nil")
+	ErrUserServiceIsNil  = New("UserService is nil")
+	ErrUserHandlerIsNil  = New("UserHandler is nil")
+	ErrRepoProviderIsNil = New("RepoProvider is nil")
+
+	// Empty string argument errors
+	ErrExpiresAtIsEmpty = New("Expiration time is empty")
+	ErrPasswordIsEmpty  = New("Password is empty")
+	ErrSessionIdIsEmpty = New("Token is empty")
+	ErrUserIdEmpty      = New("User ID is empty")
+
+	// Database errors
+	ErrUserNotFound = New("User not found")
+
 	ErrCouldNotIncrementFailedLogins = New("Could not increment users.failed_login_attempts")
 	ErrCouldNotUpdateUser            = New("Tried to update user but no changes were made")
-	ErrDuplicateEmail                = New("Duplicate email")
-	ErrEmailIsEmpty                  = New("Email is empty")
-	ErrEmailMaxLength                = New("Email exceeds max length of 254 characters")
-	ErrExpiresAtIsEmpty              = New("Expiration time is empty")
-	ErrFailedUserUpade               = New("Could not update user")
-	ErrInvalidLogin                  = New("Invalid login credentials")
-	ErrPasswordIsEmpty               = New("Password is empty")
-	ErrSessionAlreadyExists          = New("Session already exists")
-	ErrSessionIsNil                  = New("Session is nil")
-	ErrTokenGeneration               = New("Could not generate token")
-	ErrTokenIsEmpty                  = New("Token is empty")
-	ErrUserIdEmpty                   = New("User ID is empty")
-	ErrUserIsNil                     = New("User is nil")
-	ErrUserNotFound                  = New("User not found")
+	ErrMissingCredentials            = New("Requires both email and password")
 )
