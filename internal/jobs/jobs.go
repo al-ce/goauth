6ce36a156dacea3b2a9a79656c1e5bb907d344b8
al-ce <aceserani@gmail.com>
2025-05-21 11:04:01 -0500

    feat(jobs): add async job runner module; unlock accounts every config.AccountUnlockPeriod

---
+++ internal/jobs/jobs.go
@@ -0,0 +1,64 @@
+package jobs
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+
+	"godiscauth/internal/repository"
+	"godiscauth/pkg/config"
+)
+
+// StartJobs starts jobs with a context from main
+func StartJobs(ctx context.Context, wg *sync.WaitGroup, db *gorm.DB) {
+
+	// NOTE: Add goroutines here for any future jobs
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		UnlockExpiredLocks(ctx, config.AccountUnlockPeriod, db)
+	}()
+}
+
+// UnlockExpiredLocks calls the repo method to unlock all accounts whose
+// locks have expired every `period`
+func UnlockExpiredLocks(
+	ctx context.Context,
+	period time.Duration,
+	db *gorm.DB,
+) {
+	ur, err := repository.NewUserRepository(db)
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("[Jobs] [ERROR] Could not init user repo: %s", err.Error()))
+		return
+	}
+	ticker := time.NewTicker(period)
+	defer ticker.Stop()
+
+	// Perform an initial check before starting the ticker
+	unlockHelper(ur)
+
+	for {
+		select {
+		case <-ticker.C:
+			unlockHelper(ur)
+		case <-ctx.Done():
+			log.Info().Msg("[Jobs] [UnlockExpiredLocks] Stopping job")
+			return
+		}
+	}
+}
+
+func unlockHelper(ur *repository.UserRepository) {
+	affected, err := ur.UnlockAllExpiredLocks()
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("[Jobs] [ERROR] %s", err.Error()))
+	} else {
+		log.Info().Msg(fmt.Sprintf("[Jobs] [UnlockExpiredLocks] %d rows affected", affected))
+	}
+}
