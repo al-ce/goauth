ead545e2718049f75e591267663fadc7009bb201
al-ce <aceserani@gmail.com>
2025-05-21 09:51:55 -0500

    chore(models/user): add docstring, comments, use custom errors

--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -12,6 +12,7 @@ import (
 	"godiscauth/pkg/config"
 )
 
+// User represents a user in the `users` table.
 type User struct {
 	ID                  uuid.UUID  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	Email               string     `gorm:"type:varchar(255);not null;unique"`
@@ -22,23 +23,26 @@ type User struct {
 	AccountLockedUntil  *time.Time `gorm:"type:timestamp"`
 }
 
+// NewUser creates a new User value from an email and password.
 func NewUser(email string, password string) (*User, error) {
+	// Hash password
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
 	if err != nil {
 		return nil, err
 	}
 
+	// Validate email
 	if _, err := mail.ParseAddress(email); err != nil {
-		return nil, err
+		return nil, apperrors.ErrEmailFormat
 	}
 
 	if len(email) > 254 {
 		return nil, apperrors.ErrEmailMaxLength
 	}
 
+	// Enforce minimum password complexity
 	if err = passwordvalidator.Validate(password, config.MinEntropyBits); err != nil {
-		return nil, err
+		return nil, apperrors.ErrPasswordComplexity
 	}
 
 	return &User{Email: email, Password: string(hash)}, err
