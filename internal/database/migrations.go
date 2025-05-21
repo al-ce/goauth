f1d9a4bc0d329f1c3434284271c86001f1cdf6f5
al-ce <aceserani@gmail.com>
2025-05-21 10:04:59 -0500

    refactor(migrations): return error on failure over log.Fatal; add docstring, comments

--- internal/database/migrations.go
+++ internal/database/migrations.go
@@ -7,16 +7,25 @@ import (
 	"godiscauth/internal/models"
 )
 
-func Migrate(db *gorm.DB) {
+// Migrate automigrates the database according to the User and Session models
+func Migrate(db *gorm.DB) error {
+	// uuid extension
 	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`).Error; err != nil {
 		log.Fatal().Err(err).Msg("Error migrating database")
+		return err
 	}
 
+	// make User migrations
 	if err := db.AutoMigrate(&models.User{}); err != nil {
 		log.Fatal().Err(err).Msg("Error migrating User model")
+		return err
 	}
 
+	// make Session migrations
 	if err := db.AutoMigrate(&models.Session{}); err != nil {
 		log.Fatal().Err(err).Msg("Error migrating Session model")
+		return err
 	}
+
+	return nil
 }
