138a950c63b15bdf74a2c24aff798f02c9ef5231
al-ce <aceserani@gmail.com>
2025-05-21 11:02:14 -0500

    feat(repo/user): validate email/user id

--- internal/repository/userRepository.go
+++ internal/repository/userRepository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"errors"
-	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -28,10 +26,10 @@ func NewUserRepository(db *gorm.DB) (*UserRepository, error) {
 
 // RegisterUser inserts a new user into the `users` table
 func (ur *UserRepository) RegisterUser(u *models.User) error {
+	// Validate user
 	if u == nil {
 		return apperrors.ErrUserIsNil
 	}
-
 	if u.Email == "" {
 		return apperrors.ErrEmailIsEmpty
 	}
@@ -40,21 +38,19 @@ func (ur *UserRepository) RegisterUser(u *models.User) error {
 	}
 
 	err := ur.DB.Create(u).Error
-	if err != nil && strings.Contains(err.Error(), `duplicate key value violates unique constraint "users_pkey"`) {
-		return apperrors.ErrDuplicateEmail
-	}
 	return err
 }
 
 // GetUserByEmail gets a user in the database by email
 func (r *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+	if email == "" {
+		return nil, apperrors.ErrEmailIsEmpty
+	}
+
 	var user models.User
 
 	result := r.DB.First(&user, "email = ?", email)
 	if result.Error != nil {
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			return nil, apperrors.ErrUserNotFound
-		}
 		return nil, result.Error
 	}
 
@@ -63,6 +59,10 @@ func (r *UserRepository) GetUserByEmail(email string) (*models.User, error) {
 
 // GetUserByID gets a user in the database by user ID (uuid as string)
 func (r *UserRepository) GetUserByID(userID string) (*models.User, error) {
+	if userID == "" {
+		return nil, apperrors.ErrUserIdEmpty
+	}
+
 	var user models.User
 	id, err := uuid.Parse(userID)
 	if err != nil {
@@ -71,9 +71,6 @@ func (r *UserRepository) GetUserByID(userID string) (*models.User, error) {
 
 	result := r.DB.First(&user, "id = ?", id)
 	if result.Error != nil {
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			return nil, apperrors.ErrUserNotFound
-		}
 		return nil, result.Error
 	}
 
@@ -82,12 +79,18 @@ func (r *UserRepository) GetUserByID(userID string) (*models.User, error) {
 
 // PermanentlyDeleteUser removes existing users from the database by ID
 func (r *UserRepository) PermanentlyDeleteUser(userID string) (int64, error) {
+	if userID == "" {
+		return 0, apperrors.ErrUserIdEmpty
+	}
 	result := r.DB.Unscoped().Where("id = ?", userID).Delete(&models.User{})
 	return result.RowsAffected, result.Error
 }
 
 // UpdateUser updates a user in the database by usedID, expecting a decoded request to pass updated fields
 func (r *UserRepository) UpdateUser(userID string, request map[string]any) error {
+	if userID == "" {
+		return apperrors.ErrUserIdEmpty
+	}
 	var exists bool
 	r.DB.Model(&models.User{}).Select("1").Where("id = ?", userID).First(&exists)
 	if !exists {
@@ -109,11 +112,18 @@ func (r *UserRepository) UpdateUser(userID string, request map[string]any) error
 // IncrementFailedLogins increments failed login attempts and locks account every
 // `config.MaxLoginAttempts` failed attempts.
 func (r *UserRepository) IncrementFailedLogins(userID string) error {
+	// Validate user ID
+	if userID == "" {
+		return apperrors.ErrUserIdEmpty
+	}
+
+	// Lookup user
 	user, err := r.GetUserByID(userID)
 	if err != nil {
 		return apperrors.ErrUserNotFound
 	}
 
+	// Increment login attempts
 	result := r.DB.Model(&models.User{}).Where("id = ?", user.ID).
 		Updates(map[string]any{"failed_login_attempts": user.FailedLoginAttempts + 1})
 
@@ -121,6 +131,7 @@ func (r *UserRepository) IncrementFailedLogins(userID string) error {
 		return result.Error
 	}
 	if result.RowsAffected == 0 {
+		// Unsure under what circumstances this could happen, but handle the case anyway
 		return apperrors.ErrCouldNotIncrementFailedLogins
 	}
 
@@ -129,6 +140,11 @@ func (r *UserRepository) IncrementFailedLogins(userID string) error {
 
 // LockAccount locks a user account until the time spec'd in `config`
 func (r *UserRepository) LockAccount(userID string) error {
+	// Validate user ID
+	if userID == "" {
+		return apperrors.ErrUserIdEmpty
+	}
+
 	result := r.DB.Model(&models.User{}).Where("id = ?", userID).
 		Updates(map[string]any{
 			"account_locked":       true,
