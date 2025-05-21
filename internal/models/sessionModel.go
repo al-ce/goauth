df2efd10d2a9472aa4634bd543d706d4dc838972
al-ce <aceserani@gmail.com>
2025-05-21 09:48:50 -0500

    refactor!(models/session): remove Token field

--- internal/models/sessionModel.go
+++ internal/models/sessionModel.go
@@ -9,32 +9,30 @@ import (
 	"godiscauth/pkg/config"
 )
 
+// Session represents a session in the `sessions` table
 type Session struct {
 	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	UserID    uuid.UUID `gorm:"type:uuid;not null;index"`
 	User      *User     `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
-	Token     string    `gorm:"type:text;not null;unique"`
 	ExpiresAt time.Time `gorm:"type:timestamp;not null"`
 	CreatedAt time.Time `gorm:"type:timestamp;not null;default:now()"`
 }
 
-// NewSession creates a new Session value from a user id, a token string, and
-// an expiration time
-func NewSession(userID uuid.UUID, token string, expiresAt time.Time) (*Session, error) {
+// NewSession creates a new Session value from a user id, a session id, and an expiration time
+func NewSession(userID uuid.UUID, sessionID uuid.UUID, expiresAt time.Time) (*Session, error) {
 	if userID == uuid.Nil {
 		return nil, apperrors.ErrUserIdEmpty
 	}
-	if token == "" {
-		return nil, apperrors.ErrTokenIsEmpty
+	if sessionID == uuid.Nil {
+		return nil, apperrors.ErrSessionIdIsEmpty
 	}
 	if expiresAt.IsZero() {
 		return nil, apperrors.ErrExpiresAtIsEmpty
 	}
 
 	return &Session{
-		ID:        uuid.New(),
 		UserID:    userID,
-		Token:     token,
+		ID:        sessionID,
 		ExpiresAt: expiresAt,
 		CreatedAt: time.Now(),
 	}, nil
