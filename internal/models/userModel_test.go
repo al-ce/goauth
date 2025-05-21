3dfd8305982dedd500d1d813eefee399a0ecc922
al-ce <aceserani@gmail.com>
2025-05-21 10:04:09 -0500

    test(model/user): use testing password from utils; check for user nil; rename vars

--- internal/models/userModel_test.go
+++ internal/models/userModel_test.go
@@ -11,32 +11,41 @@ import (
 	"godiscauth/pkg/apperrors"
 )
 
+// TestNewUser tests new user creation in the `models` package.
 func TestNewUser(t *testing.T) {
 	is := is.New(t)
 
 	// Valid email and password should return nil err, non-nil user value
 	validEmail := "test@newuser.com"
-	validPassword := config.TestingPassword
 	t.Run("valid email and password", func(t *testing.T) {
-		user, err := models.NewUser(validEmail, validPassword)
+		user, err := models.NewUser(validEmail, testutils.TestingPassword)
 		is.True(user != nil)
 		is.NoErr(err)
 
 		// User value holds passed email and password
 		is.Equal(user.Email, validEmail)
-		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(validPassword))
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(testutils.TestingPassword))
 		is.NoErr(err)
+
 	})
 
-	exceeds254 := "example@"
-	for range 256 {
-		exceeds254 += "a"
-	}
-	exceeds254 += ".com"
+	// Emails should not exceed 254 chars per RFC3696
+	// Long emails should return error, nil user value
+	t.Run("email exceeds 254 chars", func(t *testing.T) {
+		exceeds254Chars := "example@"
+		for range 256 {
+			exceeds254Chars += "a"
+		}
+		exceeds254Chars += ".com"
 
-	invalidEmails := map[string]string{
+		user, err := models.NewUser(exceeds254Chars, testutils.TestingPassword)
+		is.Equal(user, nil)
+		is.True(err == apperrors.ErrEmailMaxLength)
+	})
+
+	// Invalid email formats should return error, nil user value
+	invalidFormats := map[string]string{
 		"emptyString":     "",
-		"exceeds254":      exceeds254,
 		"missingAt":       "example.com",
 		"multipleAts":     "at@at@at.com",
 		"missingLocal":    "@at.com",
@@ -47,13 +56,15 @@ func TestNewUser(t *testing.T) {
 		"space":           "example @at.com",
 	}
 
-	for name, email := range invalidEmails {
+	for name, email := range invalidFormats {
 		t.Run(name, func(t *testing.T) {
-			_, err := models.NewUser(email, validPassword)
+			user, err := models.NewUser(email, testutils.TestingPassword)
+			is.Equal(user, nil)
 			is.True(err != nil)
 		})
 	}
 
+	// Insufficiently complex passwords should return error, nil user value
 	invalidPasswords := map[string]string{
 		"emptyString": "",
 		"tooShort":    "short",
